687403dc91eb246778d95692919d52706fbdc276
Vincent Lambert <v.lambert@eurelis.com>
2021-01-05 14:30:28 +0100

    Test PHP - Kata 8

---
+++ kata_php/kata_08.php
@@ -0,0 +1,24 @@
+<?php
+
+# L5
+
+# Write a function that, when given a list of elements, will return the "last survivor".
+#
+# Basically you have to assume that n people are put into a circle and that they are eliminated in steps of k elements, like this:
+# solution_kata_08(7,3) => means 7 people in a circle;
+# one every 3 (step) is eliminated until one remains
+# [1,2,3,4,5,6,7] - initial sequence (list)
+# [1,2,4,5,6,7] => 3 is counted out
+# [1,2,4,5,7] => 6 is counted out
+# [1,4,5,7] => 2 is counted out
+# [1,4,5] => 7 is counted out
+# [1,4] => 5 is counted out
+# [4] => 1 counted out, 4 is the last element - the survivor!
+
+
+function solution_kata_08($list, $step) {
+    //
+    // Your code here
+    //
+    return;
+}
